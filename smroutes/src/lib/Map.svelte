8add0b7ad37678557cf082471f19bf64d4b49695
Alexey Mishkin <lexamishkin@mail.ru>
2023-12-04 16:58:55 +0400

    Намечен базовыйинтерфейс для пк

---
+++ smroutes/src/lib/Map.svelte
@@ -0,0 +1,51 @@
+<script>
+    import maplibregl from 'maplibre-gl';
+    import { onMount } from 'svelte';
+    import 'maplibre-gl/dist/maplibre-gl.css';
+    let map;
+    let samara_point = new maplibregl.LngLat(50.22125, 53.2415);
+    let mapLanguage="ru";
+    const bounds = [
+        [50.05, 53.14],
+        [50.3,53.26]
+    ];
+
+    onMount(() => {
+        map = new maplibregl.Map({
+            container: 'map',
+            style: 'https://api.maptiler.com/maps/5a2e698d-bf96-462a-a122-fbd86ebc7aae/style.json?key=r9k1CXRP7YCqI9zWOIjp',
+            center:  samara_point,
+            maxBounds: bounds,
+            zoom: 10
+        });
+
+        map.addControl(
+        new maplibregl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true
+            })
+        );
+        map.setLayoutProperty('')
+     })
+
+    function locate() {
+
+    }
+</script>
+
+<div id="map"></div>
+
+<style>
+    #map {
+        position: absolute;
+        left: 0;
+        top: 40px;
+        bottom: 0;
+        right: 0;
+    }
+    .maplibregl-ctrl-top-right {
+        right:0px;
+    }
+</style>
---
+++ smroutes/src/routes/+page.svelte
@@ -0,0 +1,44 @@
+<script>
+    import "carbon-components-svelte/css/white.css";
+    import {
+        Header,
+        SideNav,
+        SideNavItems,
+        SideNavLink,
+        SkipToContent,
+        Content,
+        Button,
+        ExpandableTile,
+        Search,
+        HeaderUtilities,
+        HeaderAction
+    } from "carbon-components-svelte";
+    import Favorite from "carbon-icons-svelte/lib/Favorite.svelte";
+    import Map from '../lib/Map.svelte';
+    let isSideNavOpen = false;
+    let expandableTileExpanded = false;
+</script>
+
+<main class="d-flex">
+    <Header company="SMROUTES" bind:isSideNavOpen>
+        <svelte:fragment slot="skip-to-content">
+          <SkipToContent />
+        </svelte:fragment>
+        <HeaderUtilities>
+            <Button icon={Favorite} iconDescription="Избранные остановки" tooltipPosition="left"/>
+          </HeaderUtilities>
+    </Header>
+    
+    <SideNav bind:isOpen={isSideNavOpen}>
+        <SideNavItems>
+            <SideNavLink>
+                <Search>
+
+                </Search>
+            </SideNavLink>
+        </SideNavItems>
+    </SideNav>
+    <Content>
+        <Map/>
+    </Content>
+</main>
