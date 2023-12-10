c7bd282097ee68ae6525496dd37f979b17ac6c96
lexxamcode <lexamishkin@mail.ru>
2023-12-10 19:44:03 +0400

    Фикс полуения данных из API

--- smroutes/src/lib/Stop.svelte
+++ smroutes/src/lib/Stop.svelte
@@ -1,4 +1,6 @@
 <script context="module">
+    import {XMLHttpRequest} from "xhr2";
+
     export class SmroutesObject {
         constructor(id, title, coords) {
             this.id = id;
@@ -21,46 +23,48 @@
 };
 
     export function getStopsFromXml() {
-        let url = 'https://tosamara.ru/api/v2/classifiers/stopsFullDB.xml';
+        let stopsXmlList = null;
+        var x = new XMLHttpRequest();
+        x.open("GET", "https://tosamara.ru/api/v2/classifiers/stopsFullDB.xml", [false, null, null]);
+        x.send();
+            
+        x.onload = function() {
+            console.log(`Загружено: ${x.status}`);
+            stopsXmlList = doc.getElementsByTagName('stops');
+        };
 
-    let request = new XMLHttpRequest();
-    request.open("GET", url, false);
-    request.send();
+        let stops = [];
+        var doc = x.response;
 
-    let xml = request.responseXML;
+        for (let i = 0; i < stopsXmlList.length; i++) {
+            let stop = stopsXmlList[i];
 
-    let stops = [];
-    let stopsXmlList = xml.getElementsByTagName('stop');
+            let id = stop.getElementsByTagName('KS_ID')[0].childNodes[0].nodeValue;
+            let title = stop.getElementsByTagName('title')[0].childNodes[0].nodeValue;
+            let coords = [
+                stop.getElementsByTagName('latitude')[0].childNodes[0].nodeValue,
+                stop.getElementsByTagName('longitude')[0].childNodes[0].nodeValue
+            ]
+            let adjacentStreet = stop.getElementsByTagName('adjacentStreet')[0].childNodes[0].nodeValue
+            let direction = stop.getElementsByTagName('direction')[0].childNodes[0].nodeValue;
 
-    for (let i = 0; i < stopsXmlList.length; i++) {
-        let stop = stopsXmlList[i];
+            let transport = {};
+            transport["busesMunicipal"] = stop.getElementsByTagName('busesMunicipal')[0].childNodes[0].nodeValue;
+            transport["busesCommercial"] = stop.getElementsByTagName('busesCommercial')[0].childNodes[0].nodeValue;
+            transport["busesPrigorod"] = stop.getElementsByTagName('busesPrigorod')[0].childNodes[0].nodeValue;
+            transport["busesSeason"] = stop.getElementsByTagName('busesSeason')[0].childNodes[0].nodeValue;
+            transport["busesSpectial"] = stop.getElementsByTagName('busesSpecial')[0].childNodes[0].nodeValue;
+            transport["busesIntercity"] = stop.getElementsByTagName('busesIntercity')[0].childNodes[0].nodeValue;
+            transport["trams"] = stop.getElementsByTagName('trams')[0].childNodes[0].nodeValue;
+            transport["trolleybuses"] = stop.getElementsByTagName('trolleybuses')[0].childNodes[0].nodeValue;
+            transport["metros"] = stop.getElementsByTagName('metros')[0].childNodes[0].nodeValue;
+            transport["electricTrains"] = stop.getElementsByTagName('electricTrains')[0].childNodes[0].nodeValue;
+            transport["riverTransports"] = stop.getElementsByTagName('riverTransport')[0].childNodes[0].nodeValue;
 
-        let id = stop.getElementsByTagName('KS_ID')[0].childNodes[0].nodeValue;
-        let title = stop.getElementsByTagName('title')[0].childNodes[0].nodeValue;
-        let coords = [
-            stop.getElementsByTagName('latitude')[0].childNodes[0].nodeValue,
-            stop.getElementsByTagName('longitude')[0].childNodes[0].nodeValue
-        ]
-        let adjacentStreet = stop.getElementsByTagName('adjacentStreet')[0].childNodes[0].nodeValue
-        let direction = stop.getElementsByTagName('direction')[0].childNodes[0].nodeValue;
+            stops.append(new Stop(id, title, coords, adjacentStreet, direction, transport));
+        }
 
-        let transport = {};
-        transport["busesMunicipal"] = stop.getElementsByTagName('busesMunicipal')[0].childNodes[0].nodeValue;
-        transport["busesCommercial"] = stop.getElementsByTagName('busesCommercial')[0].childNodes[0].nodeValue;
-        transport["busesPrigorod"] = stop.getElementsByTagName('busesPrigorod')[0].childNodes[0].nodeValue;
-        transport["busesSeason"] = stop.getElementsByTagName('busesSeason')[0].childNodes[0].nodeValue;
-        transport["busesSpectial"] = stop.getElementsByTagName('busesSpecial')[0].childNodes[0].nodeValue;
-        transport["busesIntercity"] = stop.getElementsByTagName('busesIntercity')[0].childNodes[0].nodeValue;
-        transport["trams"] = stop.getElementsByTagName('trams')[0].childNodes[0].nodeValue;
-        transport["trolleybuses"] = stop.getElementsByTagName('trolleybuses')[0].childNodes[0].nodeValue;
-        transport["metros"] = stop.getElementsByTagName('metros')[0].childNodes[0].nodeValue;
-        transport["electricTrains"] = stop.getElementsByTagName('electricTrains')[0].childNodes[0].nodeValue;
-        transport["riverTransports"] = stop.getElementsByTagName('riverTransport')[0].childNodes[0].nodeValue;
-
-        stops.append(new Stop(id, title, coords, adjacentStreet, direction, transport));
-    }
-
-    return stops;
+        return stops;
     };
 
     export function makeStopsLayer(stops) {
