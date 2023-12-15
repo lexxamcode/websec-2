efdce9e86d531856bf66df372b4cabed9015ce9c
Alexey Mishkin <lexamishkin@mail.ru>
2023-12-15 04:08:30 +0400

    Прогнозы для остановок

--- smroutes/src/lib/Map.svelte
+++ smroutes/src/lib/Map.svelte
@@ -3,71 +3,42 @@
     import  { onMount } from 'svelte';
     import 'maplibre-gl/dist/maplibre-gl.css';
     import { getStopsFromXml } from './Stop.js';
-    import Bus from "carbon-icons-svelte/lib/Bus.svelte";
 
     let map;
+    let stops;
     let samara_point = new maplibregl.LngLat(50.22125, 53.2415);
-    let stops = {
-            "name":"stops-points",
-            "type":"FeatureCollection",
-            "features":[]
-        };
-    let mapLanguage="ru";
-    const bounds = [
-        [50.05, 53.14],
-        [50.3,53.26]
-    ];
-    
 
     onMount(async () => {
         map = new maplibregl.Map({
             container: 'map',
-            style: 'https://api.maptiler.com/maps/5a2e698d-bf96-462a-a122-fbd86ebc7aae/style.json?key=r9k1CXRP7YCqI9zWOIjp',
+            style: 'https://api.maptiler.com/maps/streets-v2/style.json?key=3csEM5VieqZlKB7sM6UR',
             center:  samara_point,
             zoom: 10
         });
         map.on('load', () => {
-            map.addSource('stops', {
-                'type': 'geojson',
-                'data': stops,
-                cluster: true,
-                clusterMaxZoom: 14, // Max zoom to cluster points on
-                clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
-            });
-            
-            map.addLayer(
-            {
-                'id' : 'stops',
-                'type': 'circle',
-                'source' : 'stops',
-                'paint': 
-                {
-                    "circle-color": "red",
-                    "circle-blur": 1,
-                    "circle-opacity": 0.5,
-                    "circle-radius" : 10
-                }
-            });
-            map.addControl(
-                new maplibregl.GeolocateControl({
-                    positionOptions: {
-                        enableHighAccuracy: true
-                    },
-                    trackUserLocation: true
-                })
-            );
-            addStopsChunkByChunk();
-        })
-})
+                stops = getStopsFromXml(map);
+                map.addControl(
+                    new maplibregl.GeolocateControl({
+                        positionOptions: {
+                            enableHighAccuracy: true
+                        },
+                        trackUserLocation: true
+                    })
+                );
+        });
 
-async function addStopsChunkByChunk() {
-    let stopChunks = await getStopsFromXml();
-    for (let stopChunk of stopChunks) {
-        stops.features.push(stopChunk);
-        (map.getSource('stops')).setData(stops);
+    });
+
+    export async function findStop(title) {
+        for (let i = 0; i < stops.features.length; i++) {
+            if (title === stops.features[i].properties.title) {
+                map.flyTo({
+                    center: stops.features[i].geometry.coordinates,
+                    essential: true
+                });
+            }
+        }
     }
-}
-
 </script>
 
 <div id="map"></div>
--- smroutes/src/lib/Stop.js
+++ smroutes/src/lib/Stop.js
@@ -1,69 +1,116 @@
 import * as convert from 'xml-js';
-import maplibregl from 'maplibre-gl';
+import sha1 from 'js-sha1';
 
-    export class SmroutesObject {
-        constructor(id, title, coords) {
-            this.id = id;
-            this.title = title;
-            this.coords = coords;
-        }
-    }
-    export class Stop extends SmroutesObject {
-        constructor(id,
-                title,
-                coords,
-                adjacentStreet,
-                direction,
-                transport) {
-            super(id, title, coords);
-            this.adjacentStreet = adjacentStreet;
-            this.direction = direction;
-            this.transport = transport;
-    }
-};
+export async function getStopsFromXml(map) {
+    const response = await fetch('https://tosamara.ru/api/v2/classifiers/stopsFullDB.xml');
+    const responseText = await response.text();
 
-    export async function getStopsFromXml() {
-        const response = await fetch('https://tosamara.ru/api/v2/classifiers/stopsFullDB.xml');
-        const responseText = await response.text();
-
-        let stops = [];
-        let jsonDocument = JSON.parse(convert.xml2json(responseText, {compact: true, spaces: 4}));
-        let jsonStops = (jsonDocument['stops'])['stop'];
-        
-        for (let i = 0; i < Object.keys(jsonStops).length; i++) {
-            let stop = jsonStops[i];
-            let id = stop['KS_ID']['_text'];
-            let title = stop['title']['_text'];
-            let coords = [
-                stop['latitude']['_text'],
-                stop['longitude']['_text']
-            ]
-            let adjacentStreet = stop['adjacentStreet']['_text']
-            let direction = stop['direction']['_text'];
-
-            let transport = {};
-            transport['busesMunicipal'] = stop['busesMunicipal']['_text'];
-            transport['busesCommercial'] = stop['busesCommercial']['_text'];
-            transport['busesPrigorod'] = stop['busesPrigorod']['_text'];
-            transport['busesSeason'] = stop['busesSeason']['_text'];
-            transport['busesSpectial'] = stop['busesSpecial']['_text'];
-            transport['busesIntercity'] = stop['busesIntercity']['_text'];
-            transport['trams'] = stop['trams']['_text'];
-            transport['trolleybuses'] = stop['trolleybuses']['_text'];
-            transport['metros'] = stop['metros']['_text'];
-            transport['electricTrains'] = stop['electricTrains']['_text'];
-            transport['riverTransports'] = stop['riverTransports']['_text'];
-
-            stops[i] = new Stop(id, title, coords, adjacentStreet, direction, transport);
-        }
-        return makeStopsLayer(stops);
+    let markers =[];
+    let stops = {
+        "name":"stops-points",
+        "type":"FeatureCollection",
+        "features":[]
     };
+    let jsonDocument = JSON.parse(convert.xml2json(responseText, {compact: true, spaces: 4}));
+    let jsonStops = (jsonDocument['stops'])['stop'];
+    
+    for (let i = 0; i < Object.keys(jsonStops).length; i++) {
+        stops.features.push({ 
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [parseFloat(jsonStops[i]['longitude']['_text']), parseFloat(jsonStops[i]['latitude']['_text'])]
+            },
+            "properties": {
+                "id": jsonStops[i]['KS_ID']['_text'],
+                "title": jsonStops[i]['title']['_text'],
+                "adjacentStreet": jsonStops[i]['adjacentStreet']['_text'],
+                "direction": jsonStops[i]['direction']['_text'],
+                "transport": {
+                    "busesMunicipal":  jsonStops[i]['busesMunicipal']['_text'],
+                    "busesCommercial": jsonStops[i]['busesCommercial']['_text'],
+                    "busesPrigorod": jsonStops[i]['busesPrigorod']['_text'],
+                    "busesSeason": jsonStops[i]['busesSeason']['_text'],
+                    "busesSpecial": jsonStops[i]['busesSpecial']['_text'],
+                    "busesIntercity": jsonStops[i]['busesIntercity']['_text'],
+                    "trams": jsonStops[i]['trams']['_text'],
+                    "trolleybuses": jsonStops[i]['trolleybuses']['_text'],
+                    "metros": jsonStops[i]['metros']['_text'],
+                    "electricTrains": jsonStops[i]['electricTrains']['_text'],
+                    "riverTransports": jsonStops[i]['riverTransports']['_text'],
+                }
+            }
+        });
+        let feature = stops.features[i];
+        const featureProperties = feature.properties;
+        const popupContent = 
+        `
+            <p>${featureProperties.title}</p>
+            <p>Направление:</p>
+            <h>${featureProperties.direction}</h>
+            <h>Прогноз</h>
+            <textarea readonly id='arrival'></textarea>
+            <h>Весь транспорт</h>
+            <select id='transport'>
+                <option>Тип транспорта</option>
+                <option value='busesMunicipal'>Муниципальный</option>
+                <option value='busesCommercial'>Коммерческий</option>
+                <option value='busesPrigorod'>Пригородный</option>
+                <option value='busesSeason'>Сезонный</option>
+                <option value='busesSpecial'>Специальный</option>
+                <option value='busesIntercity'>Межгород</option>
+                <option value='trams'>Трамваи</option>
+                <option value='trolleybuses'>Троллейбусы</option>
+                <option value='metros'>Метро</option>
+                <option value='electricTrains'>Электропоезда</option>
+                <option value='riverTransports'>Речной</option>
+            </select>
+            <textarea readonly id='selected-transport'></textarea>
+        `
 
-    export function makeStopsLayer(stops) {
-        let geojsonChunks = [];
-        for (let i = 0; i < stops.length; i++) {
-            let coords = new maplibregl.LngLat(parseFloat(stops[i].coords[1]), parseFloat(stops[i].coords[0]));
-            geojsonChunks.push({ "type": "Feature","geometry": {"type": "Point","coordinates": [coords.lng, coords.lat]}, "properties": null});
-        }
-    return geojsonChunks;
-};
+        let popup = new maplibregl.Popup()
+            .setLngLat(feature.geometry.coordinates)
+            .setHTML(popupContent)
+            .addTo(map);
+        // Загружать прогноз при нажтии на маркер
+        popup.on('open', async () => {
+            let response = await fetch(`https://tosamara.ru/api/v2/json?method=getFirstArrivalToStop&KS_ID=${featureProperties.id}&os=android&clientid=test&authkey=${sha1(featureProperties.id+"just_f0r_tests")}`);
+            let arrivalJson= await response.json();
+            let arrivals = arrivalJson.arrival;
+
+            let arrivalContent = ``;
+            for (let i = 0; i < arrivals.length; i++) {
+                var arrivalDiv = document.createElement('div');
+                arrivalContent += `${arrivals[i].type} номер ${arrivals[i].number} будет через ${Math.trunc(arrivals[i].time / 60)} ч. ${arrivals[i].time % 60} м.\n`
+            }
+
+
+            document.getElementById('arrival').value = arrivalContent;
+        });
+
+        const tansportSelectElement = document.getElementById("transport");
+        tansportSelectElement.addEventListener('change', () => {
+            let value  = featureProperties.transport[document.getElementById('transport').value];
+            if (value == null) {
+                document.getElementById('selected-transport').value = 'Нет подходящих маршрутов';
+            }
+            else
+                document.getElementById('selected-transport').value = value;
+        });
+
+        const marker = document.createElement('div');
+        marker.className = 'stopMarker';
+
+        let newMarker = new maplibregl.Marker({element: marker})
+            .setLngLat(feature.geometry.coordinates)
+            .setPopup(popup)
+            .addTo(map);
+
+    };
+    return stops, markers;
+}
+
+export async function arrivalsForStop(id) {
+   
+    return JSON.parse(responseText);
+}
